bf4e6119d3f24ae68c5042717833bf6a20609e2e
baijiam <115995751+baijiam@users.noreply.github.com>
2023-05-21 13:23:03 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,115 @@
+
+<!DOCTYPE html>
+<html itemscope itemtype='http://schema.org/WebPage' lang='zh-CN' xmlns:fb='https://www.facebook.com/2008/fbml' xmlns:og='http://ogp.me/ns#' xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<title>2017级1班的网站</title>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'>
+<meta content='width=device-width,initial-scale=1.0,user-scalable=yes,minimum-scale=1.0,maximum-scale=3.0' id='viewport' name='viewport'>
+<script defer src='/dollars/v1/sites/njyx/preview.js?without_custom_code=false' type='text/javascript'></script>
+<script>
+var _strk = _strk || []
+_strk.push = function(arg1, arg2){
+  var _arrayPush = Array.prototype.push.bind(this)
+  if (typeof arg1 === 'string' && typeof arg2 === 'function') {
+    // Support _strk.push(event, fn) for better UX
+    _arrayPush([arg1, arg2])
+  } else {
+    _arrayPush.apply(this, arguments)
+  }
+}
+window.runAfterDomBinding = {
+  add: function(name, fn) {
+    _strk.push(['Page.didMount', fn])
+  }
+}
+</script>
+
+<link rel="stylesheet" media="screen" href="https://static-assets.sxlcdn.com/_reset-4fd65582921ba7920c5a289c7198c96eb5e660d2450fc330a24277fb993b89de.css" /><link rel="stylesheet" media="screen" href="https://static-fonts-css.sxlcdn.com/css?family=ZCOOL+KuaiLe:regular|Montserrat:400,700&amp;subset=latin,latin-ext" /><link rel="stylesheet" media="screen" href="https://static-assets.sxlcdn.com/strikingly_shared/ui-953c2eb3f2d3464f0110fb20f754663d7dc891f25fa5a79a04b1bba50aaed853.css" /><link rel="stylesheet" media="screen" href="https://static-assets.sxlcdn.com/themes/s5-theme/main_v4.25fd6aa3f4800d65ee23.bundle.css" />
+<link href='https://static-assets.sxlcdn.com/images/favicon-sxl.ico' rel='shortcut icon' type='image/x-icon'>
+<link href="https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg" rel="apple-touch-icon" sizes="60x60" />
+<link href="https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg" rel="apple-touch-icon" sizes="76x76" />
+<link href="https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg" rel="apple-touch-icon" sizes="120x120" />
+<link href="https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg" rel="apple-touch-icon" sizes="152x152" />
+<meta content='2017级1班的网站' property='og:title'>
+<meta content='https://baijiam.github.io/' property='og:url'>
+<meta content='https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg' property='og:image'>
+<meta content='2017级1班的网站' property='og:site_name'>
+<meta property='og:description'>
+<meta content='2017级1班的网站' itemprop='name'>
+<meta itemprop='description'>
+<meta content='https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg' itemprop='image'>
+<meta content='summary_large_image' name='twitter:card'>
+<meta content='2017级1班的网站' name='twitter:title'>
+<meta content='' name='twitter:description'>
+<meta content='https://static-assets.sxlcdn.com/images/fb_images/default-sxl.jpg' name='twitter:image'>
+<meta name='description'>
+<meta name='keywords'>
+<meta name="strikingly-host-suffix" content="sxl.cn" />
+<meta name="support-helper" content="{&quot;permalink&quot;:&quot;njyx&quot;,&quot;id&quot;:12592618}" />
+<link rel="canonical" href="https://baijiam.github.io/" />
+
+<!-- CUSTOM_CODE_START -->
+<!-- CUSTOM_CODE_END -->
+
+
+<link href='https://static-assets.sxlcdn.com/images/favicon-sxl.ico' rel='shortcut icon' type='image/x-icon'>
+<meta name="asset-url" content="https://static-assets.sxlcdn.com" />
+<script>
+var _gaq = _gaq || [];
+/* load errorception */
+var _errs=["56557d16455c179b7c00004f"];(function(a,b){a.onerror=function(){_errs.push(arguments)};
+var d=function(){var a=b.createElement("script"),c=b.getElementsByTagName("script")[0];
+a.src="//beacon.errorception.com/"+"56557d16455c179b7c00004f"+".js";a.async=!0;
+c.parentNode.insertBefore(a,c)};a.addEventListener?a.addEventListener("load",d,!1):
+a.attachEvent("onload",d)})(window,document);
+
+// Google Analytics (Universal) Setup
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','__ga');
+
+
+
+</script>
+<script>
+  (function() {
+    window.$B || (window.$B = {});
+  
+  }).call(this);
+</script>
+<script src="https://static-assets.sxlcdn.com/detectIE-c385c24313ef0e9e4e7a1e131bf5e59f0fbd468f9f9ef44fd6739ae84ef0c0a4.js" async="async"></script>
+
+</head>
+<body class='preview  locale-zh-CN product-sxl' id='sites'>
+<div id='s-page-container'></div>
+<div id='fb-root'></div>
+<div id='app-script-root'></div>
+<div id='app-view-root'></div>
+<div class='s-cookie-notification-bar' style='position: fixed; bottom: -1000px !important;'>
+<div class='cookie-notification-title'>Cookie的使用</div>
+<span class='cookie-notification-text'>我们使用cookies来确保流畅的浏览体验。若继续，我们认为你接受使用cookies。</span>
+<div>
+<button>确认</button>
+</div>
+<a href='./pages/cookie-policy' target='_blank'>了解更多</a>
+</div>
+
+<script src="//ajax.sxlcdn.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer="defer"></script>
+<script>
+  window.jQuery || document.write('<script src="https://static-assets.sxlcdn.com/jquery-f4e2137d267f77818d966e03df031337a38003039d43f15029422ddd171e14c4.js" defer="defer"><\/script>');
+</script>
+
+<div id='s-support-widget-container'></div>
+<!-- CUSTOM_CODE_START -->
+<!-- CUSTOM_CODE_END -->
+
+<script>
+  window.pingpp = {}
+</script>
+
+<script src="https://static-assets.sxlcdn.com/i18n-2ace11ac644d0b40fb8b7cb65e9dd1e553022750e0254118dacbe1fe50735e97.js" defer="defer"></script>
+<script src="https://static-assets.sxlcdn.com/webpack/site-dll.9d237f4ee6891063c21b.js" defer="defer"></script>
+<script src="https://static-assets.sxlcdn.com/webpack/page-site-bundle.e77c7d40fb20d86ebd40.js" defer="defer"></script>
+</body>
+</html>
